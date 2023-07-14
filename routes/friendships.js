87ad6491266c873a2f61a21523781d77f908cc70
Yash Jaiswal <bitorsic@protonmail.com>
2023-07-14 15:09:15 +0530

    Separate friend list / request endpoints
    - Using separate endpoints for friend requests (/friendships/requests) and friends (/friendships/:username)
    - Now using URL parameters instead of Query parameters

--- routes/friendships.js
+++ routes/friendships.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { getDb } = require('../mongoUtil');
 const auth = require('../auth');
 
-router.put('/', auth, async (req, res) => {
+router.put('/:username', auth, async (req, res) => {
     try {
-        if (req.user.username == req.query.username) throw 403;
+        if (req.user.username == req.params.username) throw 403;
 
         const users = getDb().collection('users');
-        const user = await users.findOne({ _id: req.query.username }, { projection: { _id: 1, friends: 1 } });
+        const user = await users.findOne({ _id: req.params.username }, { projection: { _id: 1, friends: 1 } });
         if (!user) throw 404;
         if (user.friends.includes(req.user.username)) throw 409;
         
@@ -39,10 +39,10 @@ router.put('/', auth, async (req, res) => {
     }
 });
 
-router.delete('/', auth, async (req, res) => {
+router.delete('/:username', auth, async (req, res) => {
     try {
         const users = getDb().collection('users');
-        const user = await users.findOne({ _id: req.query.username }, { projection: { _id: 1, friends: 1 } });
+        const user = await users.findOne({ _id: req.params.username }, { projection: { _id: 1, friends: 1 } });
         if (!user) throw 404;
 
         const friend_requests = getDb().collection('friend_requests');
@@ -68,32 +68,40 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
-router.get('/', auth, async (req, res) => {
+router.get('/requests', auth, async (req, res) => {
     try {
-        let response = [];
+        const friend_requests = getDb().collection('friend_requests');
+        const fReqs = await friend_requests.find({ '_id.to': req.user.username }).toArray();
+        if (fReqs.length == 0) throw 404;
 
-        if (req.query.requests) {
-            const friend_requests = getDb().collection('friend_requests');
-            const fReqs = await friend_requests.find({ '_id.to': req.user.username }).toArray();
-
-            for (let i=0;i<fReqs.length;i++) {
-                response.push(fReqs[i]._id.from);
-            }
-        } else {
-            const users = getDb().collection('users');
-            let user = await users.findOne({ _id: req.user.username }, { projection: { friends: 1, _id: 0 }});
-            
-            if (!req.query.username) response = user.friends;
-            else if (user.friends.includes(req.query.username)) {
-                user = await users.findOne({ _id: req.query.username }, { projection: { friends: 1, _id: 0 }});
-                response = user.friends;
-            } else throw 403;
+        for (let i=0;i<fReqs.length;i++) {
+            fReqs[i] = fReqs[i]._id.from;
         }
 
-        res.status(200).send(response);
+        res.status(200).send(fReqs);
     } catch (e) {
         let code = 500, message = e;
-        if (e == 403) { code = e, message = "User not in your friend list" }
+        if (e == 404) { code = e, message = "No pending friend requests" }
+        res.status(code).send(message);
+    }
+});
+
+router.get('/:username', auth, async (req, res) => {
+    try {
+        const users = getDb().collection('users');
+        let user = await users.findOne({ _id: req.user.username }, { projection: { friends: 1 }});
+
+        if (req.params.username != user._id) {
+            if (!(user.friends.includes(req.params.username))) throw 403;
+            user = await users.findOne({ _id: req.params.username }, { projection: { friends: 1, _id: 0 }});
+        }
+        
+        if (user.friends.length == 0) throw 404;
+        res.status(200).send(user.friends);            
+    } catch (e) {
+        let code = 500, message = e;
+        if (e == 403) { code = e, message = "User is not in your friend list" }
+        if (e == 404) { code = e, message = "Your friend list is empty" }
         res.status(code).send(message);
     }
 });
